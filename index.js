32bafc6e2adc8feac7adc56a65b50a2c320e6eab
kim-seungmin <ksm981204@naver.com>
2022-03-14 09:44:34 +0900

    postman, Mongo DB

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User")
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://ksm:tmdals6265@cluster0.wqi0v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(()=>console.log('MongoDB Connected...'))
@@ -8,6 +16,19 @@ mongoose.connect('mongodb+srv://ksm:tmdals6265@cluster0.wqi0v.mongodb.net/myFirs
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res)=>{
+  //회원가입 할떄 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다 
+
+  const user = new User(req.body)
+
+  user.save((err,userInfo) => {
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
