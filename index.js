790fb32c87aa1d29f8081832f8135564f5fd98c5
kim-seungmin <ksm981204@naver.com>
2022-03-20 09:11:33 +0900

    add cookie token, login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser=require('cookie-parser');
 
 const config = require('./config/key');
 
@@ -11,7 +12,7 @@ const {User} = require("./models/User");
 app.use(bodyParser.urlencoded({extended: true}));
 //application/json
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI).then(()=>console.log('MongoDB Connected...'))
@@ -32,6 +33,31 @@ app.post('/register', (req, res)=>{
       success: true
     })
   })
+})
+
+app.post('/login',(req, res)=>{
+  //find email from db
+  User.findOne({email: req.body.email },(err, user)=>{
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message:"제공된 이메일에 해당되는 유저가 없습니다"
+      })
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch ) =>{
+      if(!isMatch)
+        return res.json({loginSuccess:false, message: "비밀번호가 틀렸습니다"})
+      //make token
+      user.generateToken((err, user)=>{
+        if(err) return res.status(400).send(err);
+              
+      res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess:true, userId: user._id})       
+      })
+    })
+  }) 
 })
 
 app.listen(port, () => {
