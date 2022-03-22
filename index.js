72df109f62306d64cab8a132852d12da4da4a837
kim-seungmin <ksm981204@naver.com>
2022-03-22 22:57:02 +0900

    auth

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser=require('cookie-parser');
 
 const config = require('./config/key');
 
+const {auth} = require('./middleware/auth')
 const {User} = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -22,7 +23,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!~')
 })
 
-app.post('/register', (req, res)=>{
+app.post('api/users/register', (req, res)=>{
   //회원가입 할떄 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다 
 
   const user = new User(req.body)
@@ -35,7 +36,7 @@ app.post('/register', (req, res)=>{
   })
 })
 
-app.post('/login',(req, res)=>{
+app.post('api/users/login',(req, res)=>{
   //find email from db
   User.findOne({email: req.body.email },(err, user)=>{
     if(!user){
@@ -58,6 +59,19 @@ app.post('/login',(req, res)=>{
       })
     })
   }) 
+})
+
+app.post('api/users/auth', auth, (req,res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role ===0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name;
+    lastname: req.user.lastname;
+    role: req.user.role,
+    image: req.user.image
+  })
 })
 
 app.listen(port, () => {
