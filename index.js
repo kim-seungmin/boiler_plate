d43c9b2fc359daf882f1b5e30cab13b70fad9d27
kim-seungmin <54826101+kim-seungmin@users.noreply.github.com>
2022-08-08 17:06:14 +0900

    몽고DB연결

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://ksm:tmdals6265@cluster0.wqi0v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(()=>console.log('MongoDB Connected...'))
+.catch(err=>console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
